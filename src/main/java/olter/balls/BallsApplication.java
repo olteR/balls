f3fc55cf33151c02dcd27344c270ce685cb99da3
olteR <olterver@gmail.com>
2023-01-13 22:14:05 +0100

    empty project

---
+++ src/main/java/olter/balls/BallsApplication.java
@@ -0,0 +1,13 @@
+package olter.balls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BallsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/olter/balls/BallsApplicationTests.java
@@ -0,0 +1,13 @@
+package olter.balls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BallsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
