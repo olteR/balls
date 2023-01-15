e0f75ba33f3add9943669b79a294e371536591e1
olteR <olterver@gmail.com>
2023-01-15 11:12:23 +0100

    added LoginResponse

--- src/main/java/olter/balls/users/user/UserController.java
+++ src/main/java/olter/balls/users/user/UserController.java
@@ -3,6 +3,7 @@ package olter.balls.users.user;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import olter.balls.users.security.dtos.LoginRequest;
+import olter.balls.users.security.dtos.LoginResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,12 +17,12 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/users/login")
-    public ResponseEntity<String> loginUser(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+    public ResponseEntity<LoginResponse> loginUser(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
         return userService.loginUser(loginRequest);
     }
 
     @PostMapping("/users/refresh")
-    public ResponseEntity<String> refreshUser(@RequestBody String jwt, HttpServletResponse response) {
+    public ResponseEntity<LoginResponse> refreshUser(@RequestBody String jwt, HttpServletResponse response) {
         return userService.refreshUser(jwt);
     }
 }
--- src/main/java/olter/balls/users/user/UserService.java
+++ src/main/java/olter/balls/users/user/UserService.java
@@ -3,6 +3,7 @@ package olter.balls.users.user;
 import lombok.RequiredArgsConstructor;
 import olter.balls.users.security.JwtHandler;
 import olter.balls.users.security.dtos.LoginRequest;
+import olter.balls.users.security.dtos.LoginResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.GrantedAuthority;
@@ -24,32 +25,29 @@ public class UserService implements UserDetailsService {
     private final PasswordEncoder passwordEncoder;
     private final JwtHandler jwtHandler;
 
-    public ResponseEntity<String> loginUser(LoginRequest loginRequest) {
-        UserEntity user = userRepository.findByName(loginRequest.getName())
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found with this email"));
+    public ResponseEntity<LoginResponse> loginUser(LoginRequest loginRequest) {
+        UserEntity user = userRepository.findByName(loginRequest.getName()).orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found with this name"));
 
         if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid password");
         }
-        return ResponseEntity.ok().body(jwtHandler.generateJwt(user.getEmail(), new HashMap<>()));
+        return ResponseEntity.ok().body(new LoginResponse(user.getId(), user.getName(), jwtHandler.generateJwt(user.getEmail(), new HashMap<>())));
     }
 
-    public ResponseEntity<String> refreshUser(String jwt) {
+    public ResponseEntity<LoginResponse> refreshUser(String jwt) {
         try {
-            Optional<UserEntity> user = userRepository.findByName(jwtHandler.getUsernameFromToken(jwt));
-            return ResponseEntity.ok().body(jwtHandler.generateJwt(user.get().getName(), new HashMap<>()));
+            UserEntity user = userRepository.findByName(jwtHandler.getUsernameFromToken(jwt)).orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found with this name"));
+            return ResponseEntity.ok().body(new LoginResponse(user.getId(), user.getName(), jwtHandler.generateJwt(user.getEmail(), new HashMap<>())));
         } catch (Exception e) {
             if (e.getMessage().contains("JWT expired")) {
-                return ResponseEntity.ok().body("INVALID");
-            }
-            else throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Authorization failed");
+                return ResponseEntity.ok().body(new LoginResponse(null, null, "INVALID"));
+            } else throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Authorization failed");
         }
     }
 
     @Override
     public UserDetails loadUserByUsername(String name) {
-        return userEntityToUserDetails(userRepository.findByName(name)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found with this email")));
+        return userEntityToUserDetails(userRepository.findByName(name).orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found with this email")));
     }
 
     private UserDetails userEntityToUserDetails(UserEntity userEntity) {
