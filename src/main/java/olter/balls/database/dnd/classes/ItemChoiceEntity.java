ebb7e2c7963636cd8d7155f8fe2e5d8882251023
olteR <olterver@gmail.com>
2023-01-21 12:31:34 +0100

    this did not get staged for some reason

--- src/main/java/olter/balls/database/dnd/classes/ItemChoiceEntity.java
+++ src/main/java/olter/balls/database/dnd/classes/ItemChoiceEntity.java
@@ -1,2 +1,28 @@
-package olter.balls.database.dnd.classes;public class ItemChoiceEntity {
+package olter.balls.database.dnd.classes;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import olter.balls.common.BaseEntity;
+import olter.balls.database.dnd.items.MundaneItemEntity;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Table(name = "dnddb_item_choices")
+public class ItemChoiceEntity extends BaseEntity {
+    @ManyToOne
+    @JoinColumn(name = "class_id")
+    private ClassEntity classWithChoice;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "dnddb_class_item_choice",
+            joinColumns = {@JoinColumn(name = "choice_id")},
+            inverseJoinColumns = {@JoinColumn(name = "item_id")})
+    private List<MundaneItemEntity> itemChoices;
 }
