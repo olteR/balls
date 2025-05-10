f2af95d4a24b7a014731a644a545972c9c3cf1ba
olteR <olterver@gmail.com>
2025-05-10 09:12:18 +0200

    added moving lines

--- Assets/Scripts/Input/TokenControls.cs
+++ Assets/Scripts/Input/TokenControls.cs
@@ -9,19 +9,34 @@ public class TokenControls : MonoBehaviour
 
     private Vector3 _screenPoint;
     private Vector3 _offset;
+    private LineRenderer _lineRenderer;
+
+    private void Awake()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+    }
 
     void OnMouseDown() {
         _offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z));
+        _lineRenderer.positionCount = 2;
+        _lineRenderer.SetPosition(0, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0));
+        _lineRenderer.SetPosition(1, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0));
     }
 
     void OnMouseDrag() {
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z);
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + _offset;
-        transform.position = curPosition;
+        Vector3 currScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, _screenPoint.z);
+        Vector3 currPosition = Camera.main.ScreenToWorldPoint(currScreenPoint) + _offset;
+        transform.position = currPosition;
+        if (Input.GetMouseButtonDown(1)) {
+            _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, snapToGridSize(new Vector3(currPosition.x, currPosition.y, 0)));
+            _lineRenderer.positionCount++;
+        }
+        _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, new Vector3(currPosition.x, currPosition.y, 0));
     }
 
     void OnMouseUp() {
         transform.position = snapToGridSize(transform.position);
+        _lineRenderer.positionCount = 0;
     }
 
     private Vector3 snapToGridSize(Vector3 position) {
